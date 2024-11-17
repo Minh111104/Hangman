bef81ce9529e0eb5059f7555a36ae4e64ed44ba3
Minh Nguyen <121152335+Minh111104@users.noreply.github.com>
2024-11-17 11:53:30 -0500

    Add files via upload

---
+++ hangman_words.h
@@ -0,0 +1,15 @@
+#ifndef HANGMAN_WORDS_H
+#define HANGMAN_WORDS_H
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const vector<string> word_list = {
+    "abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward",
+    "axiom", "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper",
+    "bikini", "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful",
+    "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodiac", "zombie"
+};
+
+#endif // HANGMAN_WORDS_H
